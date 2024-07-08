84c3328ba876f26481dda0911f2c084dc65bdc31
Angus Townsley <angustownsley@gmail.com>
2024-07-08 11:39:27 +0100

    Complete Auth exercise

--- src/router.js
+++ src/router.js
@@ -1,17 +1,58 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const express = require('express')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const secret = process.env.SECRET_KEY
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/register', async (req, res) => {
-    
-});
+    const { username, password } = req.body
+
+    if(!username || !password) {
+        return res.status(400).send({error:"Missing fields in the request body"})
+    }
+    const userExists = await prisma.user.findUnique({
+        where: { username: username },
+    })
+
+    if (userExists) {
+        return res.status(409).send({ error: 'User already exists' })
+    }
+    const hash = await bcrypt.hash(password, 8)
+    const user = await prisma.user.create({
+        data: {
+            username: username,
+            password: hash,
+        },
+    })
+    return res.status(201).send({ user })
+})
 
 router.post('/login', async (req, res) => {
-    
-});
+    const { username, password } = req.body
+    try {
+        const user = await prisma.user.findUniqueOrThrow({
+            where: { username: username },
+        })
 
-module.exports = router;
+        const hasMatchingHash = await bcrypt.compare(password, user.password)
+
+        if (!hasMatchingHash) {
+            return res.status(401).send({ error: 'Password incorrect' })
+        }
+
+        const token = await jwt.sign(username, secret)
+
+        return res.status(201).send({ token })
+    } catch (e) {
+        if (e.code === 'P2025') {
+            return res.status(404).send({ error: 'User not found' })
+        }
+
+        return res.status(500).send({ error: e.message })
+    }
+})
+
+module.exports = router
